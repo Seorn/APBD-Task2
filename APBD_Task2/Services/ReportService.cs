61eba6b78c17fc2d52a5091886c06d02ee87fb67
Seorn <soner444433@gmail.com>
2026-03-24 01:39:26 +0100

    Added seperate report service for summaryt output

---
+++ APBD_Task2/Services/ReportService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using APBD_Task2.Models;
+
+namespace APBD_Task2.Services
+{
+    public class ReportService
+    {
+        public void PrintEquipmentSummary(List<Equipment> equipment)
+        {
+            Console.WriteLine("- SUMMARY -");
+            Console.WriteLine($"Total: {equipment.Count}");
+            Console.WriteLine($"Available: {equipment.Count(e => e.Status == EquipmentStatus.Available)}");
+            Console.WriteLine($"Rented: {equipment.Count(e => e.Status == EquipmentStatus.Rented)}");
+            Console.WriteLine($"Unavailable: {equipment.Count(e => e.Status == EquipmentStatus.Unavailable)}");
+        }
+    }
+}
+
+
